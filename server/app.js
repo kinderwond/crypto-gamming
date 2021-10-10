7f515d0c6c3e4b561c820f81c2306b4ecac793c4
Ivan Nedilko <ivan.nedilko@devit-team.com>
2021-10-10 14:22:26 +0300

    add  server env

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
+require("dotenv").config()
 const express = require('express')
 const cors = require("cors")
 const {gamming} = require("./gamming")
-const port = 3022
+const port = process.env.PORT ?? 3022
 const bodyParser = require('body-parser')
 const jsonParser = bodyParser.json()
  

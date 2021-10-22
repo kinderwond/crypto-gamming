095a37fc37f45c38b8a5c8f26a73d6a4cc1880f2
Ivan Nedilko <ivan.nedilko@gmail.com>
2021-10-22 13:37:30 +0300

    add arr flat

--- server/feistel-network/feistel-network.mapper.js
+++ server/feistel-network/feistel-network.mapper.js
@@ -1,15 +1,18 @@
 const helpers = require("./fiestel-network.helpers")
 const mapTranscodeRound = (options) => {
     console.log("mapTransecodeRound", options);
-    const {text, key, mutatedKey, round} = options || {}
+    let {text, key, mutatedKey, round} = options || {}
     const res = {}
     if (text || text.length) {
+        text = flat(text)
         res.text = helpers.arrToStr(text, " ")
     } 
     if (mutatedKey || mutatedKey.length) {
+        mutatedKey = flat(mutatedKey)
         res.mutatedKey = helpers.arrToStr(mutatedKey, " ")
     } 
     if (key || key.length) {
+        key = flat(key)
         res.key = helpers.arrToStr(key, " ")
     } 
     return {
@@ -18,6 +21,12 @@ const mapTranscodeRound = (options) => {
     }
 }
 
+function flat  (arr) {
+    if (Array.isArray(arr)) {
+        return  [].concat.apply([], arr);
+    }
+    return arr
+}
 
 
 module.exports = {
